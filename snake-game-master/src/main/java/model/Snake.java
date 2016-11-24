e55bde8a19d4eb0f99431517ad1387d7b91814b2
stalcker23 <a-zenit-a@rambler.ru>
2016-11-24 18:20:36 +0300

    ///

---
+++ snake-game-master/src/main/java/model/Snake.java
@@ -0,0 +1,129 @@
+package model;
+
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake implements Runnable {
+
+
+    private static final int UP = 0;
+    private static final int LEFT = 1;
+    private static final int DOWN = 2;
+    private static final int RIGHT = 3;
+
+    private List<SnakePart> parts;
+    private int direction;
+
+    private boolean isRunning;
+
+    public Snake() {
+        direction = DOWN;
+        isRunning = true;
+        parts = new ArrayList<SnakePart>() {{
+            add(new SnakePart(0, 2, new Image("images/head.png")));
+            add(new SnakePart(0, 1, new Image("images/body.png")));
+            add(new SnakePart(0, 0, new Image("images/body.png")));
+        }};
+    }
+
+    public void turnLeft() {
+        direction += 1;
+        if (direction > RIGHT)
+            direction = UP;
+    }
+
+    public void turnRight() {
+        direction -= 1;
+        if (direction < UP)
+            direction = RIGHT;
+    }
+
+
+    public void eat() {
+        SnakePart end = parts.get(parts.size() - 1);
+        end.setImage(new Image("images/body.png"));
+        parts.add(new SnakePart(end.getX(), end.getY(), new Image("images/body.png")));
+    }
+
+    public void move() {
+
+        SnakePart head = parts.get(0);
+
+        for (int i = parts.size() - 1; i > 0; i--) {
+            SnakePart before = parts.get(i - 1);
+            SnakePart part = parts.get(i);
+            part.setX(before.getX());
+            part.setY(before.getY());
+        }
+
+
+        switch (direction) {
+            case UP:
+                head.setY(head.getY() - 1);
+                break;
+            case LEFT:
+                head.setX(head.getX() - 1);
+                break;
+            case DOWN:
+                head.setY(head.getY() + 1);
+                break;
+            case RIGHT:
+                head.setX(head.getX() + 1);
+                break;
+        }
+
+
+        if (head.getX() < 0)
+            head.setX(15);
+        if (head.getX() > 15)
+            head.setX(0);
+        if (head.getY() < 0)
+            head.setY(15);
+        if (head.getY() > 15)
+            head.setY(0);
+    }
+
+
+    public boolean checkBitten() {
+        SnakePart head = parts.get(0);
+        for (int i = 1; i < parts.size(); i++) {
+            SnakePart part = parts.get(i);
+            if (part.getX() == head.getX() && part.getY() == head.getY())
+                return true;
+        }
+        return false;
+    }
+
+    public List<SnakePart> getParts() {
+        return parts;
+    }
+
+    public void setParts(List<SnakePart> parts) {
+        this.parts = parts;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    public void setDirection(int direction) {
+        this.direction = direction;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean running) {
+        isRunning = running;
+    }
+
+
+    @Override
+    public void run() {
+        if (isRunning)
+            move();
+    }
+}
---
+++ snake-game-master/src/main/java/model/SnakePart.java
@@ -0,0 +1,40 @@
+package model;
+
+import javafx.scene.image.Image;
+
+
+public class SnakePart {
+
+    private int x, y;
+    private Image image;
+
+    public SnakePart(int x, int y, Image image) {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
